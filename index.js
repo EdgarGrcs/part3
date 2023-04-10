f9b932b707bf88ed8a3dca599bd9d8d0bdb9f023
EdgarGrcs <edgar.gricius2@gmail.com>
2023-04-10 12:00:16 +0200

    finish part 3a

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let phonebook = 
 [
@@ -99,6 +99,7 @@ app.post("/api/persons",(request,response) => {
 
     phonebook = phonebook.concat(book);
     response.json(book);
+    morgan.token("body",request => JSON.stringify(request.body));
 })
 
 
