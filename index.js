a87d3d6711139ddac191666a96bd6270e813acfc
EdgarGrcs <edgar.gricius2@gmail.com>
2023-04-16 19:34:52 +0200

    add build for frontend, change PORT to process.env

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 //app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let phonebook = 
@@ -100,7 +101,7 @@ app.post("/api/persons",(request,response) => {
 })
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT,() => {
     console.log(`Server running on port ${PORT}`);
 } )
