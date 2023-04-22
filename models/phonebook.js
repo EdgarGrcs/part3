51f23b7889d36e46926125c2bc077e618f6d400c
EdgarGrcs <edgar.gricius2@gmail.com>
2023-04-22 13:47:30 +0200

    add eslint

--- models/phonebook.js
+++ models/phonebook.js
@@ -1,13 +1,9 @@
-const mongoose = require("mongoose");
-
-mongoose.set("strictQuery", false);
-
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 const url = process.env.MONGODB_URI;
+console.log('Connecting to', url);
 
-console.log("Connecting to", url);
-
-mongoose.connect(url)
-.then(result => {
+mongoose.connect(url).then(result => {
     console.log("connected to MongoDB");
 })
 .catch((error) => {
