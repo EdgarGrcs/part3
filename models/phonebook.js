eaf5dfbf3eb8f821819b5e9228389cc30a851874
EdgarGrcs <edgar.gricius2@gmail.com>
2023-04-22 13:52:39 +0200

    finish part3

--- models/phonebook.js
+++ models/phonebook.js
@@ -4,37 +4,35 @@ const url = process.env.MONGODB_URI;
 console.log('Connecting to', url);
 
 mongoose.connect(url).then(result => {
-    console.log("connected to MongoDB");
+console.log('connected to MongoDB');
 })
 .catch((error) => {
-    console.log("error connecting to MongoDB", error.message);
+    console.log('error connecting to MongoDB', error.message);
 })
 
 const phoneSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        required: [true, "User phone number required"],
-        validate: {
-            validator: function(v){
-                return /\d{2,3}-\d{5}/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: [true, "User phone number required"],
+    validate: {
+      validator: function(v){
+        return /\d{2,3}-\d{5}/.test(v);
+},
+      message: (props) => `${props.value} is not a valid phone number!`,
     }
+}
 })
-
-phoneSchema.set("toJSON",{
-    transform:(document,returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+phoneSchema.set('toJSON',{
+  transform:(document,returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+}
 })
-
-module.exports = mongoose.model("Phonebook", phoneSchema);
+module.exports = mongoose.model('Phonebook', phoneSchema);
